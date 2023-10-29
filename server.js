6c346d21204b4aa24f73b2a14833444f916babaa
alfredo <gabriel94alfredo@gmail.com>
2023-10-29 19:14:30 -0300

    configurando repostas

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-   "/": "Curso de Node.js"
-}
-const server = http.createServer((req, res) => {
-   res.writeHead(200, { "Content_Type": "text/plain" });
-   res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
    console.log("servidor escutando");
 })
 
